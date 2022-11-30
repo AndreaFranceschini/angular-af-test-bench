c89ac97396cc76e27cf8a40772b3a3f991976c5c
AndreaFranceschini <12218181+AndreaFranceschini@users.noreply.github.com>
2022-11-30 10:11:57 +0100

    update angular 15. 7 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import './polyfills';
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+//This will tell typescript that window also contains ngRef property
+type windowExtended = Window & typeof globalThis & {
+  ngRef: NgModuleRef<AppModule>;
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+  const _window = window as windowExtended;
+  if (_window['ngRef']) {
+    _window['ngRef'].destroy();
   }
-  window['ngRef'] = ref;
+  _window['ngRef'] = ref;
 
   // Otherwise, log the boot error
-}).catch(err => console.error(err));
+}).catch((err: any) => console.error(err));
